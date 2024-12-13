b9eac382e89b4c85b80b1f6567720c69b6913f07
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-13 16:47:04 +0500

    update sqlalchemy base model.

--- app/core/database/base_model.py
+++ app/core/database/base_model.py
@@ -1,5 +1,31 @@
-from sqlalchemy.orm import DeclarativeBase
+import datetime
+
+from sqlalchemy import DateTime, MetaData
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql.expression import text
+
+from core.config import settings
 
 
 class Base(DeclarativeBase):
-    pass
+    """Base abstract class for for SQLAlchemy ORM models.
+
+    Attributes:
+        id (int): Primary key
+        created_at (datetime): Timestamp indicating when the record was created
+        updated_at (datetime): Timestamp indicating when the record was last updated
+    """
+
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.main_db.naming_convention,
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    created_on: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=text("now()")
+    )
+    updated_on: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=text("now()"), onupdate=text("now()")
+    )
