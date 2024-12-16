bdcd346c1b1e99c24f790a959fe37bbecc419f76
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-16 11:39:25 +0500

    add get sqlalchemy  adapter method for user model.

--- app/users/models/user.py
+++ app/users/models/user.py
@@ -1,7 +1,12 @@
-from fastapi_users.db import SQLAlchemyBaseUserTable
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.database import Base
 
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
     __tablename__ = "users"
+
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyUserDatabase(session, cls)
