8794747d65309e81c6a161eeecdd89d8348ae194
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-17 10:54:44 +0500

    add dependency for get access token db adapter.

---
+++ app/users/dependencies/access_tokens.py
@@ -0,0 +1,20 @@
+from typing import AsyncGenerator
+
+from fastapi import Depends
+from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyAccessTokenDatabase
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.database import db_connector
+from users.models import AccessToken
+
+
+async def get_access_token_db(
+    session: AsyncSession = Depends(db_connector.get_session),
+) -> AsyncGenerator[SQLAlchemyAccessTokenDatabase, None]:
+    """Provides initialized database adapter of Access Token model.
+
+    Args:
+        session (AsyncSession): Async database session.
+    """
+
+    yield AccessToken.get_db(session=session)
