56f2a8196c77b969b31e0ba0da373958e4e050d1
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-17 10:52:46 +0500

    add docstrings for user model class.

--- app/users/models/user.py
+++ app/users/models/user.py
@@ -5,8 +5,19 @@ from core.database import Base
 
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
+    """A class for represtation users table in the database"""
+
     __tablename__ = "users"
 
     @classmethod
-    def get_db(cls, session: AsyncSession):
+    def get_db(cls, session: AsyncSession) -> SQLAlchemyUserDatabase:
+        """Provides database adapter for user model.
+
+        Args:
+            session (AsyncSession): Async database session to connect
+
+        Returns:
+            SQLAlchemyUserDatabase: Fastapi-users sqlalchemy adapter for user model
+        """
+
         return SQLAlchemyUserDatabase(session, cls)
