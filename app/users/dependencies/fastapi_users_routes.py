1eaadfeff42949524b275cc17fb9d5bf4078763a
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-18 15:28:55 +0500

    fix User model import.

--- app/users/dependencies/fastapi_users_routes.py
+++ app/users/dependencies/fastapi_users_routes.py
@@ -1,6 +1,6 @@
 from fastapi_users import FastAPIUsers
 
-from core.database import User
+from users.models import User
 
 from .backend import authentication_backend
 from .user_manager import get_user_manager
