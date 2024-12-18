f3aa924e7c9ce212926915fc0d13d75ef598d9b8
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-18 11:44:01 +0500

    add fastapi-users routers for auth and users.

--- app/api_router.py
+++ app/api_router.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
-from users.routers import router as auth_router
+from users.auth_routes import router as auth_router
+from users.users_routes import router as users_router
 
 from core.config import settings
 
 router = APIRouter(
     prefix=settings.prefix.api_prefix,
-    tags=["Auth"],
 )
 
 router.include_router(auth_router)
+router.include_router(users_router)
--- app/users/auth_routes.py
+++ app/users/auth_routes.py
@@ -4,12 +4,21 @@ from core.config import settings
 
 from .dependencies.backend import authentication_backend
 from .dependencies.fastapi_users_routes import fastapi_users
+from .schemas import UserCreate, UserRead
 
 router = APIRouter(
     prefix=settings.prefix.auth,
     tags=["Auth"],
 )
 
+# /login
+# /logout
 router.include_router(
     router=fastapi_users.get_auth_router(authentication_backend),
+)
+
+
+# /register
+router.include_router(
+    router=fastapi_users.get_register_router(UserRead, UserCreate),
 )
