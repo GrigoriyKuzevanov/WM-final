80b7f6d7cbc5c043fc09178578c2ecf8cf74923c
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-18 09:37:03 +0500

    add type hint for user_db parameter in get_user_manager dependency fucntion.

--- app/users/dependencies/user_manager.py
+++ app/users/dependencies/user_manager.py
@@ -1,6 +1,7 @@
 from typing import AsyncGenerator
 
 from fastapi import Depends
+from fastapi_users.db import SQLAlchemyUserDatabase
 
 from users.auth.user_manager import UserManager
 
@@ -8,7 +9,7 @@ from .users import get_user_db
 
 
 async def get_user_manager(
-    user_db=Depends(get_user_db),
+    user_db: SQLAlchemyUserDatabase = Depends(get_user_db),
 ) -> AsyncGenerator[UserManager, None]:
     """Provides initialized fastapi-user UserManager object.
 
