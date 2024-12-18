7e72979498c0b1785bd0a124780fbe707011aa86
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-18 09:27:17 +0500

    create user manager dependency.

---
+++ app/users/dependencies/user_manager.py
@@ -0,0 +1,19 @@
+from typing import AsyncGenerator
+
+from fastapi import Depends
+
+from users.auth.user_manager import UserManager
+
+from .users import get_user_db
+
+
+async def get_user_manager(
+    user_db=Depends(get_user_db),
+) -> AsyncGenerator[UserManager, None]:
+    """Provides initialized fastapi-user UserManager object.
+
+    Args:
+        user_db: Initialized database adapter of User model.
+    """
+
+    yield UserManager(user_db)
