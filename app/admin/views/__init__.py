d408e42fa857f3cb27e6bd0a2ce4d72cedb88a8e
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-23 16:54:52 +0500

    add views to init file.

--- app/admin/views/__init__.py
+++ app/admin/views/__init__.py
@@ -1,3 +1,7 @@
-__all__ = ("UserView",)
+__all__ = ("UserView", "RoleView", "StructureView", "RelationView", "TeamView")
 
+from .relation import RelationView
+from .role import RoleView
+from .structure import StructureView
+from .team import TeamView
 from .user import UserView
