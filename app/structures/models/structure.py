f2853bd9963df4890bde2ba6bab8b5266d5f4dce
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-23 16:52:35 +0500

    add repr method for admin to structure model.

--- app/structures/models/structure.py
+++ app/structures/models/structure.py
@@ -2,6 +2,7 @@ from typing import TYPE_CHECKING
 
 from sqlalchemy import Column, ForeignKey, Table
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from starlette.requests import Request
 
 from core.models.base_model import Base
 
@@ -32,3 +33,15 @@ class Structure(Base):
     )
     relations: Mapped[list["Relation"]] = relationship(back_populates="structure")
     teams: Mapped[list["Team"]] = relationship(back_populates="structure")
+
+    async def __admin_repr__(self, request: Request) -> str:
+        """Model's representation in admin.
+
+        Args:
+            request (_type_): Request instance
+
+        Returns:
+            str: Model's string representation
+        """
+
+        return f"{self.name}"
