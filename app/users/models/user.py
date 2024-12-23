e9a53a276d210e7f03225d792f77264f5fa5ee0e
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-23 19:34:55 +0500

    add fields to user model.

--- app/users/models/user.py
+++ app/users/models/user.py
@@ -1,8 +1,9 @@
 from typing import TYPE_CHECKING
 
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy import String
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from starlette.requests import Request
 
 from core.models.base_model import Base
@@ -16,6 +17,10 @@ class User(Base, SQLAlchemyBaseUserTable[int]):
     """A class for represtation users table in the database"""
 
     __tablename__ = "users"
+
+    name: Mapped[str] = mapped_column(String(length=60))
+    last_name: Mapped[str] = mapped_column(String(length=60))
+    info: Mapped[str] = mapped_column(nullable=True)
 
     roles: Mapped[list["Role"]] = relationship(
         secondary=association_table, back_populates="users", cascade="all, delete"
