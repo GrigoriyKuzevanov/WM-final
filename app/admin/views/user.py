a3d17b49b92e761164caaac98edbc26da22eb62d
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-23 16:53:09 +0500

    create user view for admin.

--- app/admin/views/user.py
+++ app/admin/views/user.py
@@ -1,5 +1,10 @@
+from starlette.requests import Request
 from starlette_admin.contrib.sqla import ModelView
 
 
 class UserView(ModelView):
-    fields = ["id", "email"]
+    fields = ["id", "email", "is_active", "is_superuser", "is_verified", "roles"]
+    page_size = 5
+
+    def can_create(self, request: Request) -> bool:
+        return False
