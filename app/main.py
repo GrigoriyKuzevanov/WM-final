611ae056e7dd0eb8bd08af63baf00f1b1d3f9d20
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-23 16:50:57 +0500

    add starlette session middleware to fastapi app.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from admin.admin_app import admin
 from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
 
 from core.config import settings
 from core.lifespan import lifespan
@@ -10,6 +11,7 @@ from .api_router import router as api_router
 app = FastAPI(
     lifespan=lifespan,
 )
+app.add_middleware(SessionMiddleware, secret_key=settings.session_middleware.secret_key)
 
 app.include_router(api_router)
 
