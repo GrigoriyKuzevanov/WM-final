3ec2f0f07eca1105533889aba5e690e0c8954728
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-23 16:52:45 +0500

    add repr method for admin to role model.

--- app/structures/models/role.py
+++ app/structures/models/role.py
@@ -2,6 +2,7 @@ from typing import TYPE_CHECKING
 
 from sqlalchemy import Column, ForeignKey, Table
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from starlette.requests import Request
 
 from core.models.base_model import Base
 from structures.models.structure import (
@@ -41,3 +42,15 @@ class Role(Base):
     subordinates: Mapped[list["Relation"]] = relationship(
         foreign_keys="Relation.subordinate_id", back_populates="subordinate"
     )
+
+    async def __admin_repr__(self, request: Request) -> str:
+        """Model's representation in admin.
+
+        Args:
+            request (_type_): Request instance
+
+        Returns:
+            str: Model's string representation
+        """
+
+        return f"{self.name}"
