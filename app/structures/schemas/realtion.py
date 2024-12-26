80ef497c295120ac25878142b415f934d63cdcd4
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-26 11:43:33 +0500

    move structure_id field from RelationBase to RealtionOut pydantic schema.

--- app/structures/schemas/realtion.py
+++ app/structures/schemas/realtion.py
@@ -4,13 +4,13 @@ from pydantic import BaseModel
 class RelationBase(BaseModel):
     superior_id: int
     subordinate_id: int
-    structure_id: int
 
     model_config = {"from_attributes": True}
 
 
 class RelationOut(RelationBase):
     id: int
+    structure_id: int
 
 
 class RelationCreate(RelationBase):
