1b2503bb4ae2849d75e14b25c008895c6884fda6
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-12-27 10:34:57 +0500

    imports refactoring.

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -14,14 +14,13 @@ __all__ = (
 
 from meetings.models import Meeting
 from meetings.models import association_table as meetings_users_association
-from work_tasks.models import WorkTask
-
 from structures.models import (
     Relation,
     Role,
     Structure,
 )
 from users.models import AccessToken, User
+from work_tasks.models import WorkTask
 
 from .base_model import Base
 from .db_connector import db_connector
