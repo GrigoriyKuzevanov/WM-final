bcd73193467eb3e317fa4a706fc55c3bebbc4e06
Григорий Кузеванов <grkuzevanov@gmail.com>
2025-01-08 11:11:30 +0500

    add relation not found exception.

--- app/structures/exceptions/relation.py
+++ app/structures/exceptions/relation.py
@@ -3,3 +3,7 @@ from fastapi import HTTPException, status
 RelationAlreadyExists = HTTPException(
     status_code=status.HTTP_403_FORBIDDEN, detail="Relation already exists"
 )
+
+RelationNotFound = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND, detail="Relation not found"
+)
