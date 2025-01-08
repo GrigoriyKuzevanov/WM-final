e21280bdbb8715e8ba204424ccc56e0e803649fd
Григорий Кузеванов <grkuzevanov@gmail.com>
2025-01-08 11:11:53 +0500

    add delete method to relations service.

--- app/structures/services/relation.py
+++ app/structures/services/relation.py
@@ -1,6 +1,6 @@
 from structures.adapters.relation_adapter import RelationAdapter
 from structures.adapters.role_adapter import RoleAdapter
-from structures.exceptions.relation import RelationAlreadyExists
+from structures.exceptions.relation import RelationAlreadyExists, RelationNotFound
 from structures.exceptions.role import RoleNotFound
 from structures.models import Relation
 from structures.schemas.realtion import RelationCreate
@@ -55,3 +55,17 @@ class RelationService:
         return await self.relations_adapter.create_realtion_with_users_structure_id(
             relation_create_schema, structure_id
         )
+
+    async def delete_relation(self, relation_id: int) -> None:
+        """Deletes relation by provided id.
+
+        Args:
+            relation_id (int): Relation id
+        """
+
+        relation_to_delete = await self.relations_adapter.read_item_by_id(relation_id)
+
+        if not relation_to_delete:
+            raise RelationNotFound
+
+        await self.relations_adapter.delete_item(relation_to_delete)
