af6213a06ade8f831175daae1e38cd97f4e82fcf
Григорий Кузеванов <grkuzevanov@gmail.com>
2025-01-09 16:49:33 +0500

    add descriptions to roles routes.

--- app/structures/roles_routes.py
+++ app/structures/roles_routes.py
@@ -18,12 +18,39 @@ router = APIRouter(
 )
 
 
-@router.get("/my", response_model=RoleOut)
+@router.get(
+    "/my",
+    response_model=RoleOut,
+    summary="Get a user's role",
+    description="""
+    Retrieve a role of the current user. Requires authorization.
+
+    Requirements:
+    - The current user must have a role
+    """,
+)
 async def get_my_role(current_user_role: RoleOut = Depends(current_user_role)):
     return current_user_role
 
 
-@router.post("/{user_id}", response_model=RoleOut, status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/{user_id}",
+    response_model=RoleOut,
+    status_code=status.HTTP_201_CREATED,
+    summary="Create a new role",
+    description="""
+    Creates a new role by a provided schema and bound a user with provided user_id to
+    it. Requires authorization. The created role belongs to the current user role's
+    structure.
+
+    Parameters:
+    - user_id: The id of the user to bound to the created role
+
+    Requirements:
+    - The current user must be the team administrator
+    - The user with provided user id must exist
+    """,
+)
 async def create_role(
     user_id: int,
     role_input_schema: RoleCreate,
@@ -43,7 +70,17 @@ async def create_role(
     )
 
 
-@router.put("/my", response_model=RoleOut)
+@router.put(
+    "/my",
+    response_model=RoleOut,
+    summary="Update a user's role",
+    description="""
+    Updates the current user's role with provided schema. Requires authorization.
+
+    Requirements:
+    - The current user must have a role
+    """,
+)
 async def update_my_role(
     role_input_schema: RoleUpdate,
     current_user_role: Role = Depends(current_user_role),
@@ -58,7 +95,23 @@ async def update_my_role(
     )
 
 
-@router.delete("/{role_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete(
+    "/{role_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    summary="Delete a role",
+    description="""
+    Deletes a role with provided id. Requires authorization.
+
+    Parameters:
+    - role_id: The id of the role to delete
+
+    Requirements:
+    - The current user must be a team administrator
+    - The role with provided id must exist
+    - The role with provided id must not be the current user's role
+    - The role with provided id must belong to the current user's structure
+    """,
+)
 async def delete_role(
     role_id: int,
     current_user_team_admin: RoleOut = Depends(current_user_team_admin),
