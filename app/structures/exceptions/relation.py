1868832b5f386d3fc249a766890d2c715061af15
Григорий Кузеванов <grkuzevanov@gmail.com>
2025-01-09 15:31:04 +0500

    add not user structure's exception.

--- app/structures/exceptions/relation.py
+++ app/structures/exceptions/relation.py
@@ -4,6 +4,13 @@ RelationAlreadyExists = HTTPException(
     status_code=status.HTTP_403_FORBIDDEN, detail="Relation already exists"
 )
 
+
 RelationNotFound = HTTPException(
     status_code=status.HTTP_404_NOT_FOUND, detail="Relation not found"
+)
+
+
+RelationForNotYourStructure = HTTPException(
+    status_code=status.HTTP_403_FORBIDDEN,
+    detail="Can't do this action. Not your structure's relation",
 )
