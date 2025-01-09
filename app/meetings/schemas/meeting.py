36f1390a8557670628061a45968c8686ed7448be
Григорий Кузеванов <grkuzevanov@gmail.com>
2025-01-09 14:12:53 +0500

    add examples to meetings schemas.

--- app/meetings/schemas/meeting.py
+++ app/meetings/schemas/meeting.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from users.schemas import UserRead
 
 
 class MeetingBase(BaseModel):
-    topic: str
-    info: str
-    meet_datetime: datetime
+    topic: str = Field(..., example="Introduction")
+    info: str = Field(..., example="Team introduction meeting")
+    meet_datetime: datetime = Field(..., example="2025-02-10T08:30:00")
 
     model_config = {"from_attributes": True}
 
