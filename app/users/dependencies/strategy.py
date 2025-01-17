28bee2bcb52efecd2c5660930c18aed3c3a5375f
Григорий Кузеванов <grkuzevanov@gmail.com>
2025-01-17 14:24:14 +0500

    create dependecy for get fastapi users auth redis strategy.

--- app/users/dependencies/strategy.py
+++ app/users/dependencies/strategy.py
@@ -1,10 +1,12 @@
 from fastapi import Depends
+from fastapi_users.authentication import RedisStrategy
 from fastapi_users.authentication.strategy.db import (
     AccessTokenDatabase,
     DatabaseStrategy,
 )
 
 from core.config import settings
+from core.redis import redis_connector
 from users.models import AccessToken
 
 from .access_tokens import get_access_token_db
@@ -20,9 +22,22 @@ def get_database_strategy(
         of Access Token model
 
     Returns:
-        DatabaseStrategy: The strategy for managing access token.
+        DatabaseStrategy: The strategy for managing access token
     """
 
     return DatabaseStrategy(
         access_token_db, lifetime_seconds=settings.access_token.lifetime_seconds
+    )
+
+
+def get_redis_strategy() -> RedisStrategy:
+    """Provides RedisStrategy instance for handling access tokens.
+
+    Returns:
+        RedisStrategy: The strategy for managing access token
+    """
+
+    return RedisStrategy(
+        redis=redis_connector.get_client(),
+        lifetime_seconds=settings.access_token.lifetime_seconds,
     )
